4cd286af1187176cbb32e065687c29cd8d229ae2
dwandrew <danwandrew@gmail.com>
2020-11-18 15:59:53 +0000

    add html entities.js

---
+++ JS/Convert HTML entities.js
@@ -0,0 +1,31 @@
+function convertHTML(str) {
+    let temp = str.split("");
+    for (var i = 0; i < temp.length; i++) {
+      switch (temp[i]) {
+        case "<":
+          temp[i] = "&lt;";
+          break;
+        case "&":
+          temp[i] = "&amp;";
+          break;
+        case ">":
+          temp[i] = "&gt;";
+          break;
+        case '"':
+          temp[i] = "&quot;";
+          break;
+        case "'":
+          temp[i] = "&apos;";
+          break;
+      }
+    }
+    temp = temp.join("");
+    return temp;
+  }
+  
+  convertHTML("Dolce & Gabbana");
+
+
+// Intermediate Algorithm Scripting: Convert HTML Entities
+// Convert the characters &, <, >, " (double quote), and ' (apostrophe), 
+// in a string to their corresponding HTML entities.
