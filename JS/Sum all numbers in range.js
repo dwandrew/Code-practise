2eab9107e768cae4a4dfd77502e36a3fb1730891
dwandrew <danwandrew@gmail.com>
2020-11-18 13:44:42 +0000

    adding notations of task as well as String replace

--- JS/Sum all numbers in range.js
+++ JS/Sum all numbers in range.js
@@ -11,3 +11,9 @@ function sumAll(arr) {
   
   sumAll([1, 4]);
   
+//   Intermediate Algorithm Scripting: Sum All Numbers in a RangePassed
+// We'll pass you an array of two numbers. 
+// Return the sum of those two numbers plus the sum of all the numbers between them. 
+// The lowest number will not always come first.
+
+// For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
--- JS/Translate Pig Latin.js
+++ JS/Translate Pig Latin.js
@@ -16,3 +16,12 @@ function translatePigLatin(str) {
 translatePigLatin("consonant");
 translatePigLatin("glove")
 translatePigLatin("Olive")
+
+// Intermediate Algorithm Scripting: Pig LatinPassed
+// Pig Latin is a way of altering English Words. The rules are as follows:
+
+// - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add "ay" to it.
+
+// - If a word begins with a vowel, just add "way" at the end.
+
+// Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
