11381de928ef9b2a41ba3ea6dc4a5c05a8006b34
dwandrew <danwandrew@gmail.com>
2020-11-18 13:10:47 +0000

    check next friday the 13th ruby

---
+++ checkFriday.rb
@@ -0,0 +1,19 @@
+require 'Date'
+require "active_support"
+def check_friday_thirteenth(date)
+if date.day < 13 
+    first = Date.new(date.year, date.month, 13)
+    if first.strftime("%A") == 'Friday'
+        return date
+    end
+end
+
+first = Date.new(date.next_month.year, date.next_month.month, 13)
+until first.strftime("%A") == 'Friday'
+    first = Date.new(first.next_month.year, first.next_month.month, 13)
+end
+
+return first 
+end
+
+
