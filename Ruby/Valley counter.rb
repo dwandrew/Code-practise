5111170fb0621f63cde2e8073d0b6a3b8f19faee
dwandrew <danwandrew@gmail.com>
2020-11-20 13:58:23 +0000

    added valley tally.rb

---
+++ Ruby/Valley counter.rb
@@ -0,0 +1,73 @@
+def countingValleys(steps, path)
+
+    # Initial valley count to get a height array
+    array = path.split('')
+    i=0 
+    height = 0
+    # need to set initial height in array to 0 to account for starting at sea level
+    height_array = [0]
+    while i < array.length do
+        if array[i] == 'U'
+            height+=1
+            height_array.push(height)
+        else 
+            height-=1
+            height_array.push(height)
+        end
+        i+=1
+    end
+
+    # checking height array for valley start and finish
+
+j=0
+valley_start = false
+valley_end= false
+valley_tally = 0
+while j < height_array.length do
+   if height_array[j] == 0 && height_array[j+1] == -1
+    valley_start = true
+   elsif height_array[j] == -1 && height_array[j+1] == 0
+    valley_end = true
+   end
+   
+   if valley_start && valley_end
+    valley_tally+=1
+    valley_start = false
+    valley_end= false
+   end
+    j+=1  
+end
+
+# returning talley of valleys
+return valley_tally
+
+
+end
+
+# An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps, for every step it was noted if it was an uphill, , or a downhill,  step. Hikes always start and end at sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
+
+# A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+# A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+# Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+
+# Example
+
+ 
+
+# The hiker first enters a valley  units deep. Then they climb out and up onto a mountain  units high. Finally, the hiker returns to sea level and ends the hike.
+
+# Function Description
+
+# Complete the countingValleys function in the editor below.
+
+# countingValleys has the following parameter(s):
+
+# int steps: the number of steps on the hike
+# string path: a string describing the path
+# Returns
+
+# int: the number of valleys traversed
+# Input Format
+
+# The first line contains an integer , the number of steps in the hike.
+# The second line contains a single string , of  characters that describe the path.
