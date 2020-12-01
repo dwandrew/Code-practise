d1f26091b90fd83d88408c63c7ac07a0848cb9bb
dwandrew <danwandrew@gmail.com>
2020-12-01 13:33:49 +0000

    added secondary to max profit.rb

--- Ruby/max profit.rb
+++ Ruby/max profit.rb
@@ -40,3 +40,39 @@ end
 
 # 1 <= prices.length <= 3 * 10 ^ 4
 # 0 <= prices[i] <= 10 ^ 4
+
+
+def single_profit(prices)
+    profit = 0
+   i = 0
+   while i < prices.length do
+       remainder = prices.slice(i+1, prices.length-1)
+       if remainder.length > 0
+           value = remainder.max - prices[i]
+           if value > profit
+               profit = value
+           end
+       end
+       i+=1
+   end
+   profit
+   
+end
+
+# Say you have an array for which the ith element is the price of a given stock on day i.
+
+# If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+# Note that you cannot sell a stock before you buy one.
+
+# Example 1:
+
+# Input: [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+#              Not 7-1 = 6, as selling price needs to be larger than buying price.
+# Example 2:
+
+# Input: [7,6,4,3,1]
+# Output: 0
+# Explanation: In this case, no transaction is done, i.e. max profit = 0.
