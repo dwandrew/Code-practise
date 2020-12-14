dffd4e6e758e0bc1ad3dfbb8941ad82b53326716
dwandrew <danwandrew@gmail.com>
2020-12-14 15:54:05 +0000

    added memod version of cansum

--- Algorithms/cansum.js
+++ Algorithms/cansum.js
@@ -4,7 +4,7 @@
 
 const canSum = (target, numbers) => {
     if (target === 0) return true
-
+    if(target < 0) return false
     for (let num of numbers){
         const remainder = target - num
         if (canSum(remainder, numbers) === true) {
@@ -14,3 +14,29 @@ const canSum = (target, numbers) => {
 
     return false
 }
+
+
+// console.log(canSum(7, [5,3,4,7]))
+// console.log(canSum(22, [5,3,4,7]))
+// console.log(canSum(7, [5,3]))
+
+// Memoizing it.
+
+const canSum_two = (target, numbers, memo = {}) => {
+    if (target in memo) return memo[target]
+    if (target === 0) return true
+    if(target < 0) return false
+    for (let num of numbers){
+        const remainder = target - num
+        if (canSum_two(remainder, numbers, memo) === true) {
+            memo[target] = true
+            return true
+        }
+    }
+
+    return false
+}
+
+console.log(canSum(7, [5,3,4,7]))
+console.log(canSum(22, [5,3,4,7]))
+console.log(canSum(1025, [5,3]))
