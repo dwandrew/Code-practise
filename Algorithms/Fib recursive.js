6e2993a89f3a67b4a57f2d21cdba0c4895e7a7b9
dwandrew <danwandrew@gmail.com>
2020-12-14 11:25:33 +0000

    added dynamic progamming to fib recursive

--- Algorithms/Fib recursive.js
+++ Algorithms/Fib recursive.js
@@ -1,9 +1,23 @@
+// O(2^n)
+
 const fib = (n) => {
     if (n <= 2) return 1;
     return fib(n-1) + fib(n-2)
 }
 
 
-console.log(fib(6))
-console.log(fib(7))
-console.log(fib(8))
+// console.log(fib(6))
+// console.log(fib(7))
+// console.log(fib(8))
+
+// Using dynamic programming
+
+const fib_two = (n, memo = {}) => {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = fib_two(n-1, memo) + fib_two(n-2, memo)
+    return memo[n];
+}
+
+console.log(fib_two(6))
+console.log(fib_two(47))
