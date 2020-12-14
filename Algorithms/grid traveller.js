6c84fe773dbf11e921e22c8ccc6e3a4187a414e1
dwandrew <danwandrew@gmail.com>
2020-12-14 14:51:02 +0000

    added grid traveller dynamic

--- Algorithms/grid traveller.js
+++ Algorithms/grid traveller.js
@@ -13,8 +13,14 @@ console.log(gridTraveler(12,2))
 console.log(gridTraveler(4,6))
 
 const gridTraveler_two = (r,c, memo = {}) => {
+    const key = r + ',' + c
+    if (key in memo) return memo[key];
     if (r ===1 && c === 1) return 1;
     if (r === 0 || c=== 0 ) return 0;
-    
-    return gridTraveler_two(r-1, c) + gridTraveler_two(r, c-1);
+    memo[key] = gridTraveler_two(r-1, c, memo) + gridTraveler_two(r, c-1, memo);
+    return memo[key]
 }
+
+
+console.log(gridTraveler_two(100, 124))
+console.log(gridTraveler_two(18, 18))
